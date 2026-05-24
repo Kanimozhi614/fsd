31ec1a46c42c3539dff46f43ebdf9e77c389a3a6
Kanimozhi216 <kanimozhic.cse2023@citchennai.net>
2026-05-24 14:10:31 +0530

    updated port

--- backend/server.js
+++ backend/server.js
@@ -155,7 +155,7 @@ app.delete("/students/:id", (req, res) => {
         }
     );
 });
-
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const port=process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("Server running on port ${port}");
 });
